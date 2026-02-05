6b64b6e60d62dc1f694d84b2b9cc844c7b06baa4
spin3l <dev.christianlopezgarcia@gmail.com>
2026-02-05 13:48:14 +0100

    fix: integrate SonarQube

--- ocean_runner/__init__.py
+++ ocean_runner/__init__.py
@@ -1,4 +1,4 @@
 from ocean_runner.config import Config, Environment
 from ocean_runner.runner import Algorithm
 
-__all__ = [Config, Algorithm, Environment]  # type: ignore
+__all__ = ["Config", "Algorithm", "Environment"]
